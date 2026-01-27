25621aa710315357c49305f8628b943b4f7ed246
Алекса Ћетковић <cetkovic.sv77.2022@uns.ac.rs>
2026-01-27 16:22:19 +0100

    use flask cli && add app config

--- src/justkeeplivin/app.py
+++ src/justkeeplivin/app.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 from flask import Flask
 from itertools import chain
 
@@ -8,12 +10,25 @@ from .x import init_extensions
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_mapping(
-
-    )
+    load_config(app)
     init_extensions(app)
 
     for bp in chain(routes, api):
         app.register_blueprint(bp)
 
     return app
+
+def load_config(app: Flask):
+    if not (cfg := os.getenv('JKL_CONFIG', None)):
+        return
+
+    cfg = Path(cfg)
+    match cfg.suffix.lower():
+        case '.toml':
+            ...
+        case '.py':
+            app.config.from_pyfile(cfg)
+        case _:
+            raise ValueError('Config format not supported: ' + cfg.suffix)
+
+
--- src/justkeeplivin/cli.py
+++ src/justkeeplivin/cli.py
@@ -1,15 +1,7 @@
+from flask.cli import FlaskGroup
 import click
 from .app import create_app
 
-# NOTE: Probably should have a single command with options
-
-@click.group()
+@click.group(cls=FlaskGroup, create_app=create_app)
 def main():
     pass
-
-@main.command()
-@click.option('-h-', '--host', default = "0.0.0.0")
-@click.option('-p', '--port', default = 5000)
-def debug(host: str, port: int):
-    app = create_app()
-    app.run(debug=True, host=host, port=port)
